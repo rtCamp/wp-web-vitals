1b58d9d8f4c168bff5bc4ec710772bb8ef8c394b
Edi Amin <to.ediamin@gmail.com>
2021-05-28 12:53:04 +0600

    Add admin settings page

--- inc/classes/class-plugin.php
+++ inc/classes/class-plugin.php
@@ -37,6 +37,8 @@ class Plugin {
 		add_action( 'admin_bar_menu', array( $this, 'add_admin_bar_menu_items' ) );
 		add_action( 'wp_enqueue_scripts', array( $this, 'web_vitals_enqueue_scripts' ), 1 );
 		add_action( 'wp_head', array( $this, 'web_vitals_enqueue_styles' ), 1 );
+		add_action( 'admin_menu', array( $this, 'admin_menu' ) );
+		add_action( 'admin_init',  array( $this, 'register_setting' ) );
 	}
 
 	/**
@@ -72,11 +74,70 @@ class Plugin {
 		$node = array(
 			'id'     => 'web_vitals_admin_bar',
 			'parent' => 'top-secondary',
-			'title'  => '<div id="web-vitals-admin-container""></div>',
+			'title'  => '<div id="web-vitals-admin-container"></div>',
 			'href'   => '#',
 		);
 
 		$wp_admin_bar->add_node( $node );
 	}
 
+	/**
+	 * Add admin menu
+	 */
+	public function admin_menu() {
+		add_options_page(
+			__( 'WP Web Vitals', 'wp-web-vitals' ),
+			__( 'WP Web Vitals', 'wp-web-vitals' ),
+			'manage_options',
+			'wp-web-vitals',
+			array(
+				$this,
+				'admin_option_page'
+			)
+		);
+	}
+
+	/**
+	 * Admin settings page display callback.
+	 */
+	public function admin_option_page() {
+		require_once WP_WEB_VITALS_PATH . '/templates/admin-option-page.php';
+	}
+
+	/**
+	 * Register admin settings
+	 */
+	public function register_setting(){
+
+		register_setting( 'wp_web_vitals', 'wp_web_vitals_crux_api_key' );
+
+		add_settings_section(
+			'wp_web_vitals_general',
+			'',
+			'',
+			'wp-web-vitals'
+		);
+
+
+		add_settings_field(
+			'wp_web_vitals_crux_api_key',
+			__( 'Chrome UX Report API Key', 'wp-web-vitals' ),
+			array( $this, 'api_key_option_html' ),
+			'wp-web-vitals',
+			'wp_web_vitals_general',
+			array(
+				'label_for' => 'wp_web_vitals_crux_api_key',
+				'class' => 'crux-api-key',
+			)
+		);
+
+	}
+
+	/**
+	 * Admin settings page API Key option field
+	 */
+	public function api_key_option_html(){
+		$api_key = get_option( 'wp_web_vitals_crux_api_key' );
+		require_once WP_WEB_VITALS_PATH . '/templates/admin-option-api-key.php';
+	}
 }
